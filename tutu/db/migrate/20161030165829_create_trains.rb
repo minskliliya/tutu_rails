2948dca7f3a89017578e30c2b267b069a602f623
minskliliya <minsk.liliya@yandex.ru>
2016-11-08 21:51:38 +0300

    Lesson 13

--- tutu/db/migrate/20161030165829_create_trains.rb
+++ tutu/db/migrate/20161030165829_create_trains.rb
@@ -2,7 +2,7 @@ class CreateTrains < ActiveRecord::Migration[5.0]
   def change
     create_table :trains do |t|
       t.string :number
-
+     
       t.timestamps
     end
   end
--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101193512) do
+ActiveRecord::Schema.define(version: 20161106203805) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -29,12 +29,24 @@ ActiveRecord::Schema.define(version: 20161101193512) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.integer "start_station_id"
+    t.integer "end_station_id"
+    t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
+    t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
+  end
+
   create_table "trains", force: :cascade do |t|
     t.string   "number"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "current_station_id"
+    t.integer  "route_id"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
+  end
+
+  create_table "users", force: :cascade do |t|
   end
 
 end
