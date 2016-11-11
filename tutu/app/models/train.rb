188c7f49b5ba4d27bccc05f6bb75acd7634b7c38
minskliliya <minsk.liliya@yandex.ru>
2016-11-11 22:50:32 +0300

    Add function in train model

--- tutu/app/models/train.rb
+++ tutu/app/models/train.rb
@@ -5,4 +5,28 @@ class Train < ActiveRecord::Base
   has_many :vagons
 
   validates :number, presence: true
+
+  def count_vagon(type_vagon)
+    count_vagon = 0
+    self.vagons.each do |vagon|
+      if vagon.type_vagon == type_vagon
+        count_vagon +=1
+      end
+    end
+    count_vagon       
+  end
+
+  def count_place(type_vagon,type_place)
+    count_place = 0
+    self.vagons.each do |vagon|
+      if vagon.type_vagon == type_vagon
+        if type_place == "place_up" 
+          count_place += vagon.place_up
+        else
+          count_place += vagon.place_down 
+        end      
+      end
+    end
+    count_place       
+  end  
 end
