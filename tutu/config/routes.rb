159667d2b45191bd8f0f44292ce1cee59d4d5c25
minskliliya <minsk.liliya@yandex.ru>
2016-11-30 10:02:47 +0300

    2 parts for 16 lesson

--- tutu/config/routes.rb
+++ tutu/config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :trains
-  resources :routes
-  resources :vagons
+  resources :trains do
+    resources :vagons, shallow: true
+  end
 
+  resources :routes
+  
   resources :railway_stations do 
     patch :update_position, on: :member
   end
+
+  resource :search, only: [:new, :show, :edit]
      
   get 'welcome/index'
 
